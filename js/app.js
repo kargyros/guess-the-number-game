1742d3aa0798a7ad2134b987e4af8a6ea69fa52b
Kostas Argyros <kostasrgrs@gmail.com>
2023-01-10 19:49:41 -0500

    add cached element references

--- js/app.js
+++ js/app.js
@@ -4,12 +4,18 @@
 
 /*-------------------------------- Variables --------------------------------*/
 
-
+let secretNum, guessList, isWinner //their type will change late so we use let. thats why we cannot use const and why they have not been declared
 
 /*------------------------ Cached Element References ------------------------*/
 
+const form = document.querySelector('form')
+const guessInput = document.querySelector('#guess-input')
+const guessesEl = document.querySelector('#prev-guesses')
+const messageEl = document.querySelector('#message')
+const resetButton = document.querySelector('#reset-button')
+const prevGuessesMsg = document.querySelector('#prev-guesses-msg')
 
-
+console.log(form, guessInput, guessesEl, messageEl, resetButton, prevGuessesMsg)
 /*----------------------------- Event Listeners -----------------------------*/
 
 
