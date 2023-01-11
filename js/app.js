be9eb65b0f33faf659c5b7c3fa15de01523a186c
Kostas Argyros <kostasrgrs@gmail.com>
2023-01-10 21:18:53 -0500

    checked guess function, stub up, render/render error

--- js/app.js
+++ js/app.js
@@ -19,6 +19,13 @@ console.log(form, guessInput, guessesEl, messageEl, resetButton, prevGuessesMsg)
 /*----------------------------- Event Listeners -----------------------------*/
 form.addEventListener('reset', init)
 
+form.addEventListener('submit', function(event) {
+  event.preventDefault()
+  if (!isWinner) {
+    checkGuess(parseInt(guessInput.value))
+  }
+}) 
+
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -32,5 +39,35 @@ prevGuessesMsg.textContent = ''
 guessList = []
 isWinner = false
 secretNum = Math.floor(Math.random() * 100 + 1)
+console.log(secretNum) 
+}
 // console.log(secretNum) - just a sanity check to see if random number is being picked 
+
+function checkGuess(guess) {
+  // clear out the input for better UX
+  guessInput.value = ''
+  // Evaluate whether guess is valid   
+    // is it within range?
+   // is it a number?
+  if (isNaN(guess) || guess < 1 || guess > 100) {
+    renderError ('Whoops! Please enter a number between 1 and 100.')
+    return
+    // Check if we have a winner
+  } else if (guess === secretNum) {
+    isWinner = true
+  }
+  // add guess to guess list if non of the above conditions are met
+  guessList.push(guess)
+  //render
+  render()
+}
+
+
+function renderError(){
+console.log('renderError invoked')
+console.log(guessList)
+}
+
+function render(){
+  console.log('render invoked')
 }
